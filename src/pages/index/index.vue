bde1a0fcec4bef4c45f7177d612b846f72de5b58
Robben <robbenwadlay@163.com>
2024-12-26 10:05:26 +0800

    add logger page

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,11 +2,13 @@
   <view class="content">
     <text class="title">{{ title }}</text>
     <view>ENV: {{ env }}</view>
+    <LoggerPage />
   </view>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import LoggerPage from './logger.vue';
 const env = import.meta.env.VITE_ENV_VARIABLE;
 
 const title = ref('Hello');
