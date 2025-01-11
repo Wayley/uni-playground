ca65b173d2c2660ab51b30f86d63a89301c98950
Robben <robbenwadlay@163.com>
2025-01-11 15:11:59 +0800

    add pages

---
+++ src/pages/device-detail/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <view>DeviceId: {{ deviceId }}</view>
+</template>
+
+<script setup lang="ts">
+import { onLoad } from '@dcloudio/uni-app';
+import { ref } from 'vue';
+
+const deviceId = ref('');
+onLoad((query) => {
+  deviceId.value = query?.id;
+});
+</script>
+<style scoped lang="scss"></style>
